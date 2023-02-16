f3b5bf27e566e02c512fd789eef16d74073bdd55
fanheer <fanheer@163.com>
2023-02-16 11:12:18 +0800

    webpack实现

--- dist/main.js
+++ dist/main.js
@@ -1,64 +1,37 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/age.js":
-/*!********************!*\
-  !*** ./src/age.js ***!
-  \********************/
-/***/ ((module) => {
-
-module.exports = "1";
-
-/***/ }),
-
-/***/ "./src/name.js":
-/*!*********************!*\
-  !*** ./src/name.js ***!
-  \*********************/
-/***/ ((module) => {
-
-module.exports = "fanheer";
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-const name = __webpack_require__(/*! ./name */ "./src/name.js");
-const age = __webpack_require__(/*! ./age */ "./src/age.js");
-console.log("姓名：", name, "年龄：", age);
-})();
-
-/******/ })()
-;
-//# sourceMappingURL=main.js.map
+     (() => {
+      var modules = {
+        
+          "./src/name.js": (module) => {
+            module.exports = "fanheer"; //给你的代码加点注释：loader2//给你的代码加点注释：loader1
+          }
+        ,
+          "./src/age.js": (module) => {
+            module.exports = "1"; //给你的代码加点注释：loader2//给你的代码加点注释：loader1
+          }
+        ,
+          "./src/index.js": (module) => {
+            const name = require("./src/name.js");
+const age = require("./src/age.js");
+console.log("姓名：", name, "年龄：", age); //给你的代码加点注释：loader2//给你的代码加点注释：loader1
+          }
+          
+      };
+      var cache = {};
+      function require(moduleId) {
+        var cachedModule = cache[moduleId];
+        if (cachedModule !== undefined) {
+          return cachedModule.exports;
+        }
+        var module = (cache[moduleId] = {
+          exports: {},
+        });
+        modules[moduleId](module, module.exports, require);
+        return module.exports;
+      }
+      var exports ={};
+      const name = require("./src/name.js");
+const age = require("./src/age.js");
+console.log("姓名：", name, "年龄：", age); //给你的代码加点注释：loader2//给你的代码加点注释：loader1
+    })();
+     
